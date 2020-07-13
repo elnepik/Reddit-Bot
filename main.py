086dc6792e6c19afdd458a95af5aa297c7258e8e
elnepik <67214151+elnepik@users.noreply.github.com>
2020-07-13 11:24:10 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import praw
+import pdb
+import re
+import os
+reddit = praw.Reddit('bot1')
+
+if not os.path.isfile("posts_replied_to.txt"):
+    posts_replied_to = []
+else:
+    with open("posts_replied_to.txt", "r") as f:
+        posts_replied_to = f.read()
+        posts_replied_to = list(filter(None, posts_replied_to))
+subreddit = reddit.subreddit("hmmm")
+
+for submission in subreddit.hot():
+    if submission.id not in posts_replied_to:
+            if re.search("hmmm", submission.title, re.IGNORECASE):
+                posts_replied_to.append(submission.id)
+                submission.reply("This is so hmmy hmmm.")
+                with open("posts_replied_to.txt", "w") as f:
+                    for post_id in posts_replied_to:
+                        f.write(post_id + "\n")
