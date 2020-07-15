c335346db8413dea4036c6c37e9c27056501a21e
elnepik <67214151+elnepik@users.noreply.github.com>
2020-07-15 09:58:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ reddit = praw.Reddit('bot1')
 if not os.path.isfile("posts_replied_to.txt"):
     posts_replied_to = []
 else:
-    with open("posts_replied_to.txt", "r") as f:
+    with open("posts_replied_to.txt", "a") as f:
         posts_replied_to = f.read()
         posts_replied_to = list(filter(None, posts_replied_to))
 subreddit = reddit.subreddit("hmmm")
@@ -20,4 +20,4 @@ for submission in subreddit.hot():
                 submission.reply("This is so hmmy hmmm.")
                 with open("posts_replied_to.txt", "w") as f:
                     for post_id in posts_replied_to:
-                        f.write(post_id + "\n")
+                        f.append(post_id + "\n")
